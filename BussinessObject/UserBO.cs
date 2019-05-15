76d8be1ad8ec1d9b4e5319a448b2d43e276b622e
tanviriqbal <tanviriqbal4u@live.com>
2019-05-15 20:18:23 +0600

    Add UserBO class in BusinessObject

---
+++ BussinessObject/UserBO.cs
@@ -0,0 +1,10 @@
+﻿namespace BussinessObject
+{
+    public class UserBO
+    { 
+        public string Name { get; set; }
+        public string address { get; set; }
+        public string EmailID { get; set; }
+        public string Mobilenumber { get; set; }
+    }
+}
