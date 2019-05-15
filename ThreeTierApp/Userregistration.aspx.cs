656f10bdee9ec9516836d9064cd7060e5b33e1e2
tanviriqbal <tanviriqbal4u@live.com>
2019-05-15 20:41:10 +0600

    Add a user registration page

---
+++ ThreeTierApp/Userregistration.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ThreeTierApp
+{
+    public partial class Userregistration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
